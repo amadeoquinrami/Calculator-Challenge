337f06e760bb3c279c1e25f9b50d4a6e8cba7492
Amadeo Ramirez <amadeoramirez@MacBookPro.lan>
2025-11-23 14:43:54 -0500

    uploading updated project

--- style.js
+++ style.js
@@ -1,37 +1,47 @@
-//  eval function in javascript
-//Displayoutput
+
 let input ="";
 const operators =['+', '-', '*', '/']
 function displayScreen(value, displayValue){
-    let lastChar = input.slice(-1);
-    if (operators.includes(lastChar) && operators.includes(value)) {
+    let lastChar = input.slice(-1); // prevents double operators
+    if (operators.includes(lastChar) && operators.includes(value)) { // findss the last charcter or operator
         return;
     }
       input += value ;
-const screen = document.querySelector(".screenOutput")
+
     if (displayValue !== undefined){ 
-        screen.textContent += displayValue;
+        UpdateDisplayScreen(displayValue);
      } else { 
-      screen.textContent = input;
-} 
+      UpdateDisplayScreen(input);
+    } 
 }
 function UpdateDisplayScreen(value) {
     const screen = document.querySelector(".screenOutput");
     screen.textContent = value;
 }
-function solve() {
-    let result = eval(input);
-    UpdateDisplayScreen(result);
-    input = result.toString();
-}
+function solve() { // handles the erros such as dividing by zeroo
+    try {
+        if (input.includes("/0")) {
+            UpdateDisplayScreen("Cannot divide by zero");
+            input = "";
+            return;
+        }
+     let result = eval(input);
+     if(!isFinite(result)){
+        UpdateDisplayScreen("Math ERROR");
+        input = "";
+        return;
+     }
+     UpdateDisplayScreen(result);
+     input = result.toString();
+
+    }catch (error) {
+    UpdateDisplayScreen("Invalid Input");
+        input = "";
+    }
+} 
 function clearScreen() {
     input = "";
     UpdateDisplayScreen(input);
-}
-function solve() {
-    let result = eval(input);
-    UpdateDisplayScreen(result);
-    input = result.toString();
 }
 function backspace() {
     if (input.length > 0){
